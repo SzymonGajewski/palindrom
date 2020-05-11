5869b69994f545770b176e9fc21ce672b7ece425
SzymonGajewski <szymongaju@gmail.com>
2020-05-11 11:27:23 +0200

    Correct version

--- palindromeFunction.cpp
+++ palindromeFunction.cpp
@@ -1,4 +1,7 @@
 bool isPalindrome(const std::string & text){
+    int counter = 0;
     for(auto i = 0; i < (text.size()/2); i++)
-        return(text[i]==text[text.size()-i-1]);
+        if(text[i]==text[text.size()-i-1])
+            counter++;
+    return(counter==(text.size()/2));
 }
